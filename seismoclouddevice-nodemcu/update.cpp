42f8361bd21118956be9de849fbf39000b34c298
Enrico204 <enrico204@gmail.com>
2018-08-25 13:28:44 +0200

    Added commented code for HTTPs firmware update

--- seismoclouddevice-nodemcu/update.cpp
+++ seismoclouddevice-nodemcu/update.cpp
@@ -2,6 +2,27 @@
 #include "update.h"
 
 #ifndef DONT_UPDATE
+/*
+ * TODO: implement HTTPs
+ * 
+#include <WiFiClientSecure.h>
+
+WiFiClientSecure client;
+
+if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+}
+
+if (client.verify(fingerprint, host)) {
+    Serial.println("certificate matches");
+  } else {
+    Serial.println("certificate doesn't match");
+    return;
+}
+
+auto ret = ESPhttpUpdate.update(client, host, url);
+*/
 
 #ifdef IS_ESP
 // https://esp8266.github.io/Arduino/versions/2.0.0/doc/ota_updates/ota_updates.html
